de8722cb514c9295a49e5358e36cee23fc3eaec9
Greg Davis <jrobley3@hotmail.com>
2020-03-08 22:36:41 -0400

    lab finished, string part was identical to integer

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +13,44 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String[] dupesGone = new String[0];
+        String[] newArray;
+        int count = 0;
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]){
+                    count++;
+                }
+            }
+            if (count < maxNumberOfDuplications){
+                newArray = Arrays.copyOf(dupesGone, dupesGone.length + 1);
+                newArray[newArray.length - 1] = array[i];
+                dupesGone = newArray;
+            }
+            count = 0;
+        }
+        return dupesGone;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String[] exactDupesGone = new String[0];
+        String[] otherArray;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array.length; j++){
+                if(array[i] == array[j]){
+                    count++;
+                }
+            }
+            if(count != exactNumberOfDuplications){
+
+                otherArray = Arrays.copyOf(exactDupesGone,exactDupesGone.length+1);
+                otherArray[otherArray.length - 1] = array[i];
+                exactDupesGone = otherArray;
+            }
+            count = 0;
+        }
+        return exactDupesGone;
     }
 }
